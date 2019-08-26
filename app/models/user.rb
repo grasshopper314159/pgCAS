42f27f009c413d14287a0d15ea05f4a8ff792125
Nate Johnson <grasshopper314159@gmail.com>
2019-08-26 13:19:45 -0600

    Revert "Configured for CAS"
    This reverts commit f6d13f4012716e76e613197c03d73e763d89b5cb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :cas_authenticatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   enum role: [:user, :admin]
 

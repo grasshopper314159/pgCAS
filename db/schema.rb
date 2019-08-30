0b629c0fd3331d76916c29e8dac656fee371636f
Nathan Johnson <grasshopper314159@gmail.com>
2019-08-29 20:32:41 -0600

    fixing postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_144838) do
+ActiveRecord::Schema.define(version: 2019_08_27_204323) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,9 +37,9 @@ ActiveRecord::Schema.define(version: 2019_07_20_144838) do
   end
 
   create_table "assignment_users", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "assignment_id"
-    t.datetime "submit_date"
+    t.bigint "user_id"
+    t.bigint "assignment_id"
+    t.datetime "submit_date", default: -> { "now()" }
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "grade"
@@ -53,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_07_20_144838) do
     t.datetime "updated_at", null: false
     t.string "command"
     t.string "argument"
+    t.integer "assignment_type"
   end
 
   create_table "users", force: :cascade do |t|
@@ -69,4 +73,5 @@ ActiveRecord::Schema.define(version: 2019_07_20_144838) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
